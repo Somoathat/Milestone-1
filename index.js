f0bcc26b1d2c7b78e2e4e0ca4e9200652e4c9cdf
Evan Graham <evangraham19@gmail.com>
2023-07-27 21:41:43 -0700

    update 3

--- index.js
+++ index.js
@@ -51,9 +51,34 @@ function cellClicked(){
 }
 
  function checkWinner(){
+    let roundWon = fals;
 
+    for(let i = 0; i < winConditions.length; i++)
+    const condition = winConditions[i];
+    const cellA = options[condition[0]];
+    const cellB = options[condition[1]];
+    const cellC = options[condition[2]];
+    if(cellA == cellB && cellB == cellC){
+        roundWon = true;
+        break;
+    }
+    }
+ if(roundWon){
+    statusText.textContent =' ${currentPlayer} wins!'
+    running = false;
  }
+ else if (!options.includes("")){
+    statusText.textContent = 'Draw!'
+    running = false;
+ }
+  else{
+    changePlayer();
+  }
 
  function restartGame(){
-
+    currentPlayer = "X";
+    options =  ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = ${currentPlayer}'s turn;'
+    cells.forEach(cell => cell.textContent ="")
+    running = true;    
  }
